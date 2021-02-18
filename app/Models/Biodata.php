3702f1ca2bb543c36ec5114eb942e2fbc159e4e6
Setiawan Joko <setyawanj65@gmail.com>
2021-02-18 12:31:11 +0700

    Fix | Reconfigure model to match new migrations

--- app/Models/Biodata.php
+++ app/Models/Biodata.php
@@ -4,23 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Biodata extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $table = 'biodata';
 
     protected $fillable = [
-        'user_id', 'prodi_id', 'no_pendaftaran', 'nik', 'nama', 'tempat_lahir', 'tanggal_lahir', 'agama', 'jenis_kelamin', 'alamat', 'jalur_masuk', 'asal_sekolah', 'asal_jurusan', 'foto', 'status'
+        'user_id', 'no_pendaftaran', 'nik', 'nama_depan', 'nama_belakang', 'tempat_lahir', 'tanggal_lahir', 'agama', 'jenis_kelamin', 'alamat', 'no_telepon', 'jalur_masuk', 'asal_sekolah', 'asal_jurusan', 'tahun_lulurs', 'foto'
     ];
 
     public function user() {
         return $this->belongsTo(User::class, 'user_id', 'id');
-    }
-
-    public function prodi() {
-        return $this->belongsTo(Prodi::class, 'prodi_id', 'id');
     }
 
     public function wali() {

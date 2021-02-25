0c37ac1295bdae36c4620c6298f0b5ccf3b0c4e7
Setiawan Joko <setyawanj65@gmail.com>
2021-02-25 10:18:45 +0700

    Change | Change homepage with customized frontend

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('homepage');
 
 
 //    Route::get('/register', [RegisterController::class, 'index'])->name('mahasiswa.register');

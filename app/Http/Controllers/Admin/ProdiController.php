32f237da1cfe0640bf2097b5d52921e3f9221803
medtindo <medtindonesia@gmail.com>
2021-03-07 21:34:58 +0700

    gelombang , jenjang view

--- app/Http/Controllers/Admin/ProdiController.php
+++ app/Http/Controllers/Admin/ProdiController.php
@@ -3,19 +3,35 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Fakultas;
+use App\Models\Jenjang;
+use App\Models\Prodi;
 use Illuminate\Http\Request;
 
 class ProdiController extends Controller
 {
     public function index() {
-        //
-    }
+       $data = Prodi::all();
+       $dataJenjang = Jenjang::all();
+       $dataFakultas = Fakultas::all();
 
-    public function create() {
-        //
+       return response()->view('admin.master.program-studi', compact('data','dataJenjang','dataFakultas'));
     }
 
     public function store(Request $request) {
-        //
+        $data = $request->validate([
+            'nama' => 'required|string',
+            'kode_prodi' => 'string'
+        ]);
+
+        try {
+            Jenjang::create([
+                'nama' => $data['nama']
+            ]);
+
+            return response()->redirectToRoute('admin.jenjang.store');
+        } catch(\Exception $e) {
+            dd($e->getMessage());
+        }
     }
 }

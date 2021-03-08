2264902e112431d6953b10f7b9df7517dd9b303e
Setiawan Joko Prakoso <setyawanj65@gmail.com>
2021-03-08 09:01:51 +0700

    Fix | Redirect for admin

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Support\Facades\Gate;
 
 class LoginController extends Controller
 {
@@ -26,7 +27,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/biodata';//RouteServiceProvider::HOME;
+    protected $redirectTo = '/biodata';
 
     /**
      * Create a new controller instance.
@@ -35,6 +36,9 @@ class LoginController extends Controller
      */
     public function __construct()
     {
+        $gate = Gate::inspect('admin');
+
+        if($gate) $this->redirectTo = '/admin/tes-tpa';
         $this->middleware('guest')->except('logout');
     }
 }

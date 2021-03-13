fdc5b700065a35ce00d06900e37c95f8c239274a
medtindo <medtindonesia@gmail.com>
2021-03-13 22:22:52 +0700

    master page disable font

--- resources/views/admin/master/master.blade.php
+++ resources/views/admin/master/master.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="http://fonts.cdnfonts.com/css/helvetica-neue-9" rel="stylesheet">
+    {{-- <link href="http://fonts.cdnfonts.com/css/helvetica-neue-9" rel="stylesheet"> --}}
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800&display=swap"
           rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
--- resources/views/mahasiswa/master-mahasiswa.blade.php
+++ resources/views/mahasiswa/master-mahasiswa.blade.php
@@ -7,7 +7,7 @@
 
     <link rel="preconnect" href="https://fonts.gstatic.com">
 
-    <link href="http://fonts.cdnfonts.com/css/helvetica-neue-9" rel="stylesheet">
+    {{-- <link href="http://fonts.cdnfonts.com/css/helvetica-neue-9" rel="stylesheet"> --}}
 
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800&display=swap"
         rel="stylesheet">

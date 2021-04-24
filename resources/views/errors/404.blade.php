72c0be613e536d22bdb04e4818d0536f1654b22a
medtindo <medtindonesia@gmail.com>
2021-04-24 16:05:05 +0700

    Add | error page view

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,67 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" rel="stylesheet">
+    
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+    
+    <link href="{{ asset('unigres/css/main.css') }}" rel="stylesheet"/>
+    <link href="{{ asset('unigres/css/responsive.css') }}" rel="stylesheet"/>
+
+    <title>Unigres - 404</title>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-light main-nav">
+    <div class="main-container">
+        <a class="navbar-brand" href="{{ route('homepage') }}">
+            <img class="logo-brand" src="{{ asset('unigres/images/logo.png') }}">
+            <p>PMB.<span>Unigres</span></p>
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item">
+                    <a class="nav-link" aria-current="page" href="{{ route('homepage') }}">Beranda</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('pengumuman') }}">Pengumuman</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('kontak') }}">Kontak</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('register') }}">Pendaftaran</a>
+                </li>
+                <li class="nav-item btn-nav">
+                    <a class="nav-link" href="{{ route('login') }}">Login</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+<section class="content">
+    <div class="container ct-wrapp">
+        <img class="mb-4" src="{{ asset('unigres/images/404.png') }}">
+        <h1 class="not-found">Page Not Found!</h1>
+        <p class="desc-not-found">Terjadi gangguan internet atau halaman yang anda kunjungi tidak tersedia. Periksa kembali koneksi internet anda.</p>
+    </div>
+</section>
+<footer style="background-color : white;">
+    <ul class="wrapper-footer">
+        <li>Copyright © 2019 Universitas Gresik</li>
+        <li>Jl. Arif Rahman Hakim 2B, Gresik</li>
+        <li>Telp.(031) 3981918, 3978628</li>
+        <li>WA. 081 230 798 700</li>
+    </ul>
+</footer>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+</body>
+</html>

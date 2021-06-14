cb69ee827575bb91a0f98b49b58d942bab49a9bb
Setiawan Joko <setyawanj65@gmail.com>
2021-06-14 08:00:38 +0700

    Update | Update priority to match php 8

--- app/Http/Controllers/Pembayaran/DaftarUlangController.php
+++ app/Http/Controllers/Pembayaran/DaftarUlangController.php
@@ -132,7 +132,7 @@ class DaftarUlangController extends Controller
     public function nomorSurat(){
         $tahun = Carbon::today()->year;
         $count = Pembayaran::where('kategori', 'daftar_ulang')->whereNotNull('no_surat')->whereYear('created_at', $tahun)->count();
-        $seq = substr(str_repeat(0, 3).$count + 1, - 3);
+        $seq = substr(str_repeat(0, 3) . ($count + 1), - 3);
 
         return $seq . '/PAN-PMB/' . $tahun;
     }

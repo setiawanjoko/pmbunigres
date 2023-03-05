96e5093b5c01c2bf09cbf44c49c4ec485a61fa40
ZidanFikriAditya <secondakun2004@gmail.com>
2023-03-05 15:04:59 +0700

    Fix | VA deleted in checker

--- app/Http/Controllers/Administrator/Keuangan/BNIVACheckerController.php
+++ app/Http/Controllers/Administrator/Keuangan/BNIVACheckerController.php
@@ -16,9 +16,16 @@ class BNIVACheckerController extends Controller
     public function checker(Request $request) {
         $pembayaran = Pembayaran::where('custCode', $request->bniva)->first();
 
-        $addInfo = json_decode($pembayaran->add_info);
-        $response = BNIPayment::inquiryBilling($addInfo->trx_id);
+        if (is_null($pembayaran)){
+            return redirect()->route('administrator.keuangan.check.bniva')->with([
+                "status" => "danger",
+                "message" => "Virtual account tidak terdaftar di PMB Unigres"
+            ]);
+        } else {
+            $addInfo = json_decode($pembayaran->add_info);
+            $response = BNIPayment::inquiryBilling($addInfo->trx_id);
 
-        return view('administrator.keuangan.check-bniva', compact('response'));
+            return view('administrator.keuangan.check-bniva', compact('response'));
+        }
     }
 }

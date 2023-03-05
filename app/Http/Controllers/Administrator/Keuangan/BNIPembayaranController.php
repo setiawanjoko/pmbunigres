2bad12749cc23dd14b6a6aa110b7f979647d875a
ZidanFikriAditya <secondakun2004@gmail.com>
2023-03-05 10:02:43 +0700

    Update | Delete Pembayaran

---
+++ app/Http/Controllers/Administrator/Keuangan/BNIPembayaranController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers\Administrator\Keuangan;
+
+use App\Helpers\BniEnc;
+use App\Helpers\BNIPayment;
+use App\Http\Controllers\Controller;
+use App\Models\Pembayaran as model;
+use Illuminate\Http\Request;
+
+class BNIPembayaranController extends Controller
+{
+    public function delete($id){
+        $pembayaran = model::findOrFail($id);
+
+        // BNI check status
+        $addInfo = json_decode($pembayaran->add_info);
+        $bniInquiry = BNIPayment::inquiryBilling($addInfo->trx_id);
+        try {
+            if ($bniInquiry['va_status'] !== 1){
+                $pembayaran->delete();
+                return redirect()->route('administrator.keuangan.pembayaran.index')->with([
+                    'status' => 'success',
+                    'message' => 'Berhasil menghapus kode BNI.'
+                ]);
+            } else {
+                return redirect()->route('administrator.keuangan.pembayaran.index')->with([
+                    'status' => 'danger',
+                    'message' => 'Tidak dapat menghapus kode BNI. Kode BNI sudah dibayar di bayar.'
+                ]);
+            }
+
+        } catch(\Throwable $e) {
+            dd(e);
+        }
+    }
+}
